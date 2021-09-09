8f3392b6e0ec07af92fadf6f5a13e506f724b704
Anton Derevianko <derevianko@quartsoft.com>
2021-09-09 19:12:40 +0300

    Create Factories and update Seeder

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dish;
+use App\Models\Menu;
+use Exception;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MenuFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Menu::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function definition(): array
+    {
+        $dishesIdArray = Dish::all('id')->pluck('id')->toArray();
+
+        return [
+            'dish_id' => $this->faker->randomElement($dishesIdArray),
+            'price' => random_int(7, 245),
+            'actual_at' => $this->faker->dateTimeBetween('-1 day', '+1 day'),
+        ];
+    }
+}
