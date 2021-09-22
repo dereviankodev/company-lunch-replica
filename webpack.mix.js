25e787717f78f2010b7ead1b0bf8665695c043c9
Anton Derevianko <derevianko@quartsoft.com>
2021-09-22 18:48:42 +0300

    Separation of base and custom scripts and styles. Setting up Laravel mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,9 @@
-const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix
-    .js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ]);
+if (['base', 'main'].includes(process.env.npm_config_section)) {
+    require(`${__dirname}/webpack.${process.env.npm_config_section}.mix.js`)
+} else {
+    console.log(
+        '\x1b[41m%s\x1b[0m',
+        'Provide correct --section argument to build command: base, main'
+    )
+    throw new Error('Provide correct --section argument to build command!')
+}
