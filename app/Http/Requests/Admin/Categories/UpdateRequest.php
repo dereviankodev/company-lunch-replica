fdd1581c6739acc58b457579b7abfc140dc180b3
Anton Derevianko <derevianko@quartsoft.com>
2021-09-23 18:22:43 +0300

    Category request validation

---
+++ app/Http/Requests/Admin/Categories/UpdateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\Admin\Categories;
+
+use App\Models\Category;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * @property Category $category
+ */
+class UpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', 'unique:categories,id,'.$this->category->id]
+        ];
+    }
+}
