580dbe79e6fe357d6b73e8b587585210fd2e7423
Anton Derevianko <derevianko@quartsoft.com>
2021-09-30 18:17:22 +0300

    Adapt for php 7.4

--- app/GraphQL/Mutations/CartMutator.php
+++ app/GraphQL/Mutations/CartMutator.php
@@ -3,11 +3,10 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\Cart;
-use Illuminate\Database\Eloquent\Collection;
 
 class CartMutator
 {
-    public function forceDelete($root, array $args): Collection|array
+    public function forceDelete($root, array $args)
     {
         $carts = Cart::where('user_id', $args['user_id'])->get();
         Cart::where('user_id', $args['user_id'])->forceDelete();
