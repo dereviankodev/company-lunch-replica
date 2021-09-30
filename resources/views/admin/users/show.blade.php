e27e1d1b4c1e1ce6228e34cb49a666ee228b9dc3
Anton Derevianko <derevianko@quartsoft.com>
2021-09-30 17:35:14 +0300

    Added list cart and order positions. Make it Optimized!

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -38,4 +38,78 @@
         </tr>
         </tbody>
     </table>
+
+{{--    <p><a href="" class="btn btn-success">Cart</a></p>--}}
+
+    <table class="table table-bordered">
+        <thead>
+        <tr><th colspan="5">Cart positions</th></tr>
+        <tr>
+            <th>Category</th>
+            <th>Dish</th>
+            <th>Ingredients</th>
+            <th>Weight (g)</th>
+            <th>Price</th>
+            <th>Count</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @forelse($user->carts as $cart)
+            <tr>
+                <td>{{ $cart->menu->dish->category->name }}</td>
+                <td>{{ $cart->menu->dish->name }}</td>
+                <td>{{ $cart->menu->dish->ingredients }}</td>
+                <td>{{ $cart->menu->dish->weight }}</td>
+                <td>{{ $cart->menu->price }}</td>
+                <td>{{ $cart->count }}</td>
+            </tr>
+        @empty
+            <tr><td colspan="5">None</td></tr>
+        @endforelse
+
+        </tbody>
+    </table>
+
+    <table class="table table-bordered">
+        <thead>
+        <tr><th colspan="9">Orders positions</th></tr>
+        <tr>
+            <th>ID</th>
+            <th>Customer</th>
+            <th>Recipient</th>
+            <th>Category</th>
+            <th>Dish</th>
+            <th>Ingredients</th>
+            <th>Weight (g)</th>
+            <th>Price</th>
+            <th>Count</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @forelse($user->orderCustomers as $order)
+            @php($id = null)
+            @foreach($order->orderItems as $item)
+                <tr>
+                    @if($id !== $order->id)
+                    <td rowspan="{{ $order->orderItems->count() ?? 1 }}">{{ $order->id }}</td>
+                    <td rowspan="{{ $order->orderItems->count() ?? 1 }}">{{ $user->name }}</td>
+                    <td rowspan="{{ $order->orderItems->count() ?? 1 }}">{{ $order->recipient->name ?? $user->name }}</td>
+                    @endif
+                    <td>{{ $item->dish->category->name }}</td>
+                    <td>{{ $item->dish->name }}</td>
+                    <td>{{ $item->dish->ingredients }}</td>
+                    <td>{{ $item->dish->weight }}</td>
+                    <td>{{ $item->price }}</td>
+                    <td>{{ $item->count }}</td>
+                </tr>
+                @php($id = $order->id)
+            @endforeach
+        @empty
+            <tr><td colspan="9">None</td></tr>
+        @endforelse
+
+        </tbody>
+    </table>
 @endsection
