ba54407cd702bc096622a6e4a983e3b26a5cc39f
Anton Derevianko <derevianko@quartsoft.com>
2021-10-12 15:52:21 +0300

    Update Order graphql query and mutations

--- app/GraphQL/Mutations/OrderMutator.php
+++ app/GraphQL/Mutations/OrderMutator.php
@@ -4,15 +4,22 @@ namespace App\GraphQL\Mutations;
 
 use App\Models\Cart;
 use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class OrderMutator
 {
+    private int $user_id;
+
+    public function __construct(Auth $auth)
+    {
+        $this->user_id = $auth::guard()->user()->getAuthIdentifier();
+    }
+
     public function makeAutoOrder($root, array $args)
     {
-        $customerId = $args['customer_id'];
         $recipientId = $args['recipient_id'] ?? null;
-        $order = Order::create(['customer_id' => $customerId, 'recipient_id' => $recipientId]);
-        $carts = Cart::where('user_id', $args['customer_id'])->with('menu')->get();
+        $order = Order::create(['customer_id' => $this->user_id, 'recipient_id' => $recipientId]);
+        $carts = Cart::where('user_id', $this->user_id)->with('menu')->get();
         $orderItems = [];
 
         foreach ($carts as $cart) {
@@ -25,7 +32,7 @@ class OrderMutator
         }
 
         $order->orderItems()->createMany($orderItems);
-        Cart::where('user_id', $args['customer_id'])->forceDelete();
+        Cart::where('user_id', $this->user_id)->forceDelete();
 
         return $order;
     }
