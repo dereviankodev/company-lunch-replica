36b2e6458c2044bf8253d599faef8231495f2035
Anton Derevianko <derevianko@quartsoft.com>
2021-10-26 10:34:23 +0300

    Added user role check

---
+++ app/GraphQL/Queries/AmIAdministrator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AmIAdministrator
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args): bool
+    {
+        $guard = Auth::guard();
+        /** @var User $user */
+        $user = $guard->user();
+
+        return $user->is_admin;
+    }
+}
